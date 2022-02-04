59b61f497a7e14f0f556349fbf064f67749b2477
Karol Pastewski <karolpastewski29@gmail.com>
2022-02-04 18:26:14 +0100

    LinkedList initial - Push Front  and Back; Print + Examples

--- DataStructures/LinkedList.cs
+++ DataStructures/LinkedList.cs
@@ -8,6 +8,55 @@ namespace DataStructuresDotNet.DataStructures
 {
     public class LinkedList
     {
+        internal class Node
+        {
+            public int value;
+            public Node? next;
 
+            public Node(int val)
+            {
+                value = val;
+                next = null;
+            }
+        }
+
+        private Node? _head;
+        private Node? _tail;
+
+        public LinkedList()
+        {
+            _head = null;
+            _tail = null;
+        }
+
+        public void PushFront(int val)
+        {
+            Node newNode = new Node(val);
+            newNode.next = _head;
+            if (_head == null)
+                _tail = newNode;
+            _head = newNode;
+        }
+
+        public void PushBack(int val)
+        {
+            Node newNode = new Node(val);
+            if (_tail != null)
+                _tail.next = newNode;
+            else
+                _head = newNode;
+            _tail = newNode;
+        }
+
+        public void Print()
+        {
+            Node? node = _head;
+            while(node != null)
+            {
+                Console.Write($"{node.value} -> ");
+                node = node.next;
+            }
+            Console.WriteLine("null");
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DataStructuresDotNet.DataStructures;
+
+LinkedList list = new LinkedList();
+list.Print();
+list.PushFront(5);      // 5 -> null
+list.PushFront(6);      // 6 -> 5 -> null
+list.PushFront(1);      // 1 -> 6 -> 5 -> null
+list.Print();
+list.PushFront(7);      // 7 -> 1 -> 6 -> 5 -> null
+list.Print();
+list.PushBack(8);       // 7 -> 1 -> 6 -> 5 -> 8 -> null
+list.PushBack(2);       // 7 -> 1 -> 6 -> 5 -> 8 -> 2 -> null
+list.Print();
